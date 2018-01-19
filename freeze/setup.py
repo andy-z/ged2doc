d51e75a96bd0e76bb9d40e6980fa4060107ef2f8
Andy Salnikov <andy@salnikov.us>
2018-01-18 20:59:20 -0800

    Enable freezing and installing on Windows

---
+++ freeze/setup.py
@@ -0,0 +1,12 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"excludes": ["tkinter"]}
+
+setup(name="ged2doc",
+      version="0.1.3",
+      description="ged2doc command line tool",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("freeze\\freeze_main.py",
+                              targetName="ged2doc.exe",
+                              shortcutName="ged2doc")])
