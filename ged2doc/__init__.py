d53641ca7522bf55b18cae6f96d8b2b083ad588f
Andy Salnikov <andy@salnikov.us>
2018-04-02 21:07:37 -0700

    Bump version: 0.1.9 → 0.1.10

--- ged2doc/__init__.py
+++ ged2doc/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Andy Salnikov"""
 __email__ = 'ged4py@py-dev.com'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='ged2doc',
-    version='0.1.9',
+    version='0.1.10',
     description="Tools for converting GEDCOM data into document formats.",
     long_description=readme + '\n\n' + history,
     author="Andy Salnikov",
