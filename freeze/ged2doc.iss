f1e6d0c3e84dd3e4bd67c87afbb2ea6c2639772e
Andy Salnikov <andy@salnikov.us>
2020-10-09 21:57:42 -0700

    Bump version: 0.4.2 → 0.5.0

--- freeze/ged2doc.iss
+++ freeze/ged2doc.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "ged2doc"
-#define MyAppVersion "0.4.2"
+#define MyAppVersion "0.5.0"
 #define MyAppPublisher "Andy Salnikov"
 #define MyAppURL "https://github.com/andy-z/ged2doc"
 #define MyAppExeName "ged2doc.exe"
--- freeze/setup.py
+++ freeze/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"excludes": ["tkinter"]}
 
 setup(name="ged2doc",
-      version="0.4.2",
+      version="0.5.0",
       description="ged2doc command line tool",
       options={"build_exe": build_exe_options},
       executables=[Executable("freeze\\freeze_main.py",
